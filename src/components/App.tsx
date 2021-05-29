6b8454f19e5645b5288950ad0524fd72fff2c45a
Veith Bürgerhoff <it9-buerve@student.itech-bs14.de>
2021-05-29 23:44:38 +0200

    "borrowed" timeline code from material-ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import { Box, Container, Typography } from "@material-ui/core";
 import React from "react";
 import Copyright from "./Copyright";
 import Header from "./Header";
+import Timeline from "./timeline/timeline";
 
 export default class App extends React.Component {
   render(): JSX.Element {
@@ -9,6 +10,7 @@ export default class App extends React.Component {
       <div>
         <Header />
         <Container maxWidth="sm">
+          <Timeline />
           <Box my={4}>
             <Typography variant="h4" component="h1" gutterBottom>
               Portfolio starter using material-ui, react, and typescript
